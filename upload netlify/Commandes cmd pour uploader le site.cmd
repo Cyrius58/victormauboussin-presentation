70cc571f12f5bfa84af1784bc53bd1b21597cae5
FRCyrius <victor.mauboussin@outlook.fr>
2018-12-16 18:34:34 +0100

    first commit

--- upload netlify/Commandes cmd pour uploader le site.cmd
+++ upload netlify/Commandes cmd pour uploader le site.cmd
@@ -1,4 +1,4 @@
-cd C : \Users\Victor\Dropbox\CS2I\Cours\STEPHANE ROBBE\WEB\TP2\Racine
+cd C:\Users\Victor\Dropbox\CS2I\Cours\STEPHANE ROBBE\WEB\TP2\Racine
 git add .
 git commit -m "first commit"
 git push -u origin master
